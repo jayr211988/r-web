ae0072de1611db3b6347053c8ead2ab77135b2d9
Rogelio Mendeja <rmendeja@yondu.com>
2020-09-08 09:35:10 +0800

    Updates

---
+++ precache-manifest.b023a487edb7d68a8afc42b8d03bdfd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f1df33feb9ef5a2c1d5bc284ae94e8",
+    "url": "/r-web/index.html"
+  },
+  {
+    "revision": "afe1fdebb02e39cbb5ec",
+    "url": "/r-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/r-web/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/r-web/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afe1fdebb02e39cbb5ec",
+    "url": "/r-web/static/js/main.0c9e932f.chunk.js"
+  },
+  {
+    "revision": "baf8af514abeb0853c9c",
+    "url": "/r-web/static/js/runtime-main.103f1757.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/r-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
