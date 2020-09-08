dd2d991dd5f1905f0de81549eca36a9974770e40
Rogelio Mendeja <rmendeja@yondu.com>
2020-09-08 16:16:45 +0800

    my web

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+import {
+  Button, Paper, Table, TableBody, TextField,
+  TableCell, TableContainer, TableHead, TableRow
+} from '@material-ui/core';
 
 const PokemonRow = ({ pokemon, onSelect }) => (
   <tr>
     <td>{pokemon.name.english}</td>
     <td>{pokemon.type.join(", ")}</td>
     <td>
-      <button
+      <Button
+        variant="contained"
         onClick={() => onSelect(pokemon)}
       >Select
-      </button>
+      </Button>
     </td>
   </tr>
 )
@@ -26,20 +33,22 @@ PokemonRow.propTypes = {
 }
 
 const PokemonInfo = ({ name, base }) => (
-  <div>
+  <div style={{marginLeft: '2vw'}}>
     <h1>{name.english}</h1>
-    <table>
-      <tbody>
-        {
-          Object.keys(base).map((key) => (
-            <tr key={key}>
-              <td>{key}</td>
-              <td>{base[key]}</td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
+    <Paper elevation={3}>
+      <table>
+        <tbody>
+          {
+            Object.keys(base).map((key) => (
+              <tr key={key}>
+                <td>{key}</td>
+                <td>{base[key]}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </Paper>
   </div>
 )
 
@@ -57,69 +66,128 @@ PokemonInfo.propTypes = {
   })
 }
 
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const Title2 = styled.h3`
+  text-align: center;
+  color: lightgreen
+`;
+
+const TwoColumnLayout = styled.div`
+  display: grid;
+  grid-template-columns: 70% 30%;
+  grid-column-gap: 1re;
+  margin-top: 2vh;
+`;
+
+const Container = styled.div`
+  margin: auto;
+  width: 800px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`;
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 350,
+  },
+});
+
+
 
 function App() {
 
   const [filter, filterSet] = React.useState("")
   const [selectedItem, selectedItemSet] = React.useState(null)
   const [pokemons, pokemonSet] = React.useState([])
+  const [loading, loadingSet] = React.useState(false)
+  const classes = useStyles();
 
   React.useEffect(() => {
-    //https://jayr211988.github.io
-    fetch("https://jayr211988.github.io/r-web/pokemon.json")
-      .then((res) => res.json())
-      .then((data)=>pokemonSet(data))
-      
+    async function fetchData() {
+      //https://jayr211988.github.io
+      loadingSet(true)
+      fetch("https://jayr211988.github.io/r-web/pokemon.json")
+        .then((res) => {
+          return res.json()
+        })
+        .then((data) => {
+          pokemonSet(data)
+          console.log(data, 'testset');
+          loadingSet(false)
+        })
+
+    }
+    fetchData();
   }, [])
 
   return (
-    <div style={{
-      margin: 'auto',
-      width: 800
-    }}>
-      <h1 className="title">Hi! I'm Rogelio</h1>
-      <p className="title">You can search your favorite pokemen here</p>
-      <input
-        onChange={(evt) => filterSet(evt.target.value)}
-        placeholder="Enter you pokemon here"
-        value={filter}
-      />
+    <Container>
+      <Title>Hi! I'm Rogelio</Title>
+      <Title2>You can search your favorite anime character here</Title2>
+      <TextField
+        style={{width: '100%'}}
+        variant="outlined"
+        onChange={(evt) => filterSet(evt.target.value)} 
+        value={filter} 
+        label="Enter you anime character here" />
       <div>
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: '70% 30%',
-            gridColumnGap: '1rem'
-          }}
-        >
-          <table width="100%">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Type</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                pokemons
-                  .filter((pokemon) => pokemon.name.english.toLowerCase().includes(filter.toLowerCase()))
-                  .slice(0, 20)
-                  .map((pokemen) => (
-                    <PokemonRow pokemon={pokemen} key={pokemen.id}
-                      onSelect={(pokemen) => selectedItemSet(pokemen)}
-                    />
-                  ))
-              }
-            </tbody>
-          </table>
+        <TwoColumnLayout>
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell align="center">Type</TableCell>
+                  <TableCell align="center">Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {
+                  loading ?
+                    <TableRow >
+                      <TableCell component="th" scope="row">
+                        Please wait loading...
+                      </TableCell>
+                    </TableRow> :
+                    <>
+                      {pokemons
+                        .filter((pokemon) => pokemon.name.english.toLowerCase().includes(filter.toLowerCase()))
+                        .slice(0, 20)
+                        .map((pokemon) => (
+                          <TableRow key={pokemon.id}>
+                            <TableCell component="th" scope="row">
+                              {pokemon.name.english}
+                            </TableCell>
+                            <TableCell align="center">{pokemon.type.join(", ")}</TableCell>
+                            <TableCell align="center">
+                              <Button
+                                variant="contained"
+                                onClick={() => selectedItemSet(pokemon)}
+                              > Select
+                          </Button>
+                            </TableCell>
+                          </TableRow>
+                        ))}
+                    </>
+                }
+              </TableBody>
+            </Table>
+          </TableContainer>
           {
             selectedItem && (
               <PokemonInfo {...selectedItem} />
             )
           }
-        </div>
+        </TwoColumnLayout>
       </div>
-    </div>
+    </Container>
   );
 }
 
